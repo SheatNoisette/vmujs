b9941872cb7a81ef10e2820a7227ed6e0e17aa6c
SheatNoisette <sheatnoisette@protonmail.com>
2022-12-20 17:12:08 +0100

    vmujs: Simple isqrt from V demo

--- vmujstests/vmujs_call_v_fn_test.v
+++ vmujstests/vmujs_call_v_fn_test.v
@@ -1,6 +1,7 @@
 module vmujstests
 
 import vmujs
+import math
 
 // test 1 {
 
@@ -26,6 +27,39 @@ fn test_simple_vfunction_call() {
 	}
 
 	assert state.get_function_data('simple')['test'] == 'Hello World!'
+}
+
+// }
+
+// test 2 {
+
+fn simple_isqrt_callback(vm &C.js_State) {
+	// Get VMuJS state - From which it was called
+	mut state := vmujs.get_vmujs(vm)
+
+	// Get the 1st argument
+	arg := state.pop_int() or {
+		panic('(callback) failed to pop int')
+	}
+
+	// Push the result
+	state.push_int(int(math.sqrt(arg)))
+}
+
+fn test_simple_isqrt() {
+	mut state := vmujs.new_state(.strict)
+
+	state.register_function('isqrt', 1, simple_isqrt_callback)
+
+	state.push_code("var out = isqrt(9);") or {
+		panic('failed to push code')
+	}
+
+	number := state.get_global_int('out') or {
+		panic('failed to get global')
+	}
+
+	assert number == 3
 }
 
 // }
